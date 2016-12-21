b05395e09e62d09f4521b8f3df6c02f3a907bd57
Andrei <andrei.mironenko@gmail.com>
2016-12-21 19:34:23 +0000

    Experimenting ...

--- Chapter03/ver12/tictactoe/main.cpp
+++ Chapter03/ver12/tictactoe/main.cpp
@@ -1,10 +1,20 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QWidget>
+#include <QPalette>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    QPalette pall;
+    QPixmap pix(":/redstar.png");
+    QCursor cur(pix);
+    pall.setColor(w.backgroundRole(), Qt::blue);
+    w.setPalette(pall);
+    w.setAutoFillBackground(true);
+
+    w.setCursor(Qt::PointingHandCursor);
     w.show();
     return a.exec();
 }
